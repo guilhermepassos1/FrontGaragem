a81ab0e7fa87939509e09b4065caeefb53f5b750
Guilherme Passos <gepassos06@gmail.com>
2024-07-25 11:24:23 -0300

    adição das logo e redimensão das telas

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.r4sf46vbt2"
+    "revision": "0.s0cutur1cfo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
