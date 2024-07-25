c4aa7937b15d3c77b84aed188a121f6eded7caee
Guilherme Passos <gepassos06@gmail.com>
2024-07-25 10:19:22 -0300

    criação dos layouts

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ek5cq4aljto"
+    "revision": "0.r4sf46vbt2"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
